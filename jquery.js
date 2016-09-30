6ecb7055f5c7c9d013b3548eb8e419b40aaa7237
John Black <john.f.black@icloud.com>
2016-09-30 18:56:55 -0400

    update

--- jquery.js
+++ jquery.js
@@ -8,6 +8,10 @@ $(function(){
     
 /* ------ Buttons ------- */
 $("button").button();
+    
+/* ------ Table ------- */
+
+$("#myTable").DataTable();
 
     
 /* ------ Date ------ */
@@ -15,9 +19,28 @@ var d = new Date();
 document.getElementById("date").innerHTML = d.toDateString();
 
     
-/* ------ Weather ------- */
+/* ------- IA News ------- */
+    
+var feed = "https://developers.facebook.com/blog/iablog/rss?format=xml.xml";
+    $.ajax(feed, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
 
+            $(data).find("item").each(function () {
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+    
 
+        }   
+    });
+   
     
 });
 
