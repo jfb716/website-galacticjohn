eef8450aca1e09ff554103eb593378cb60469536
John Black <john.f.black@icloud.com>
2016-10-09 19:45:28 -0400

    set up stock and weather api's

--- jquery.js
+++ jquery.js
@@ -19,27 +19,35 @@ var d = new Date();
 document.getElementById("date").innerHTML = d.toDateString();
 
     
-/* ------- IA News ------- */
+/* ------- IA API ------- */
     
-var feed = "https://developers.facebook.com/blog/iablog/rss?format=xml.xml";
-    $.ajax(feed, {
-        accepts:{
-            xml:"application/rss+xml"
-        },
-        dataType:"xml",
-        success:function(data) {
-
-            $(data).find("item").each(function () {
-                var el = $(this);
-                console.log("------------------------");
-                console.log("title      : " + el.find("title").text());
-                console.log("link       : " + el.find("link").text());
-                console.log("description: " + el.find("description").text());
-            });
+$.ajax({
+  url: "https://developers.facebook.com/blog/iablog/rss",
+  dataType: "xml",
+});
+    
     
 
-        }   
-    });
+/* ------- Weather Temp API ------- */
+    
+$.ajax({
+  url : "http://api.wunderground.com/api/ecac81007093c564/conditions/q/NY/New_York.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var temp_f = parsed_json['current_observation']['temp_f'];
+$("#temp").html(temp_f);
+  }
+  });
+    
+/* ------- Stock Price API ------- */
+$.ajax({
+  url : "http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=fb",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var stock_price = parsed_json['LastPrice'];
+$("#stock_price").html(stock_price);
+  }
+  });
    
     
 });
