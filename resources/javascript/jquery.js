a9ebe6cfe027845dbd2bd7f0afc1cfaa1d81855b
John Black <john.f.black@icloud.com>
2017-01-22 15:34:54 -0500

    created dark neon styles keying off local storage key/value

--- resources/javascript/jquery.js
+++ resources/javascript/jquery.js
@@ -61,14 +61,28 @@ $("#stock_price").html(price);
   }
   });
 
-/*
-$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22FB%22)%0A%09%09&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=", function(data) {
-    var ask = data.query.results.quote.ask;
-    console.log(ask);
-    });
 
-    */
+/* ------- Dark Neon Styles off Local Storage ------- */
 
+if (localStorage.getItem("space") === "black") {
+  $("body, .navBar, .section_name, .content_box, .ad_box, a").css({
+    "background-color": "black",
+    "color": "#83F52C",
+    "border-color": "#FF6600"
+  });
+
+  $("img").css({
+    "background-color": "#83F52C"
+  });
+
+  $("#sbar").css({
+    "background-color": "#FF6600"
+  });
+
+  $("#time-field").css({
+    "color": "#FF6600"
+  });
+}
 
 
 });
