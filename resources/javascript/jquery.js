e45fc207c1dd1b958816978644807a3083c42edc
John Black <john.f.black@icloud.com>
2017-01-21 22:47:52 -0500

    fixed stock quote api code for all pages

--- resources/javascript/jquery.js
+++ resources/javascript/jquery.js
@@ -1,10 +1,10 @@
 
 
-$(document).ready(function(){   
+$(document).ready(function(){
 
 
 /* ------ Hamburger ------*/
-    
+
 if (window.matchMedia("(max-width: 500px)").matches) {
 $( ".cross" ).hide();
 $( ".navBar" ).hide();
@@ -25,23 +25,23 @@ $( ".hamburger" ).show();
 } else {
 console.log("Desktop");
 }
-    
 
-    
+
+
 /* ------ Buttons ------- */
 $("button").button();
-    
 
 
-    
+
+
 /* ------ Date ------ */
 var d = new Date();
 document.getElementById("date").innerHTML = d.toDateString();
 
-    
+
 
 /* ------- Weather Temp API ------- */
-    
+
 $.ajax({
   url : "http://api.wunderground.com/api/ecac81007093c564/conditions/q/NY/New_York.json",
   dataType : "jsonp",
@@ -50,61 +50,25 @@ $.ajax({
 $("#temp").html(temp_f);
   }
   });
-    
-/* ------- Stock Price API ------- 
+
+
 $.ajax({
   url : "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22FB%22)%0A%09%09&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=",
   dataType : "json",
   success : function(parsed_json) {
-  var price = parsed_json['Ask'];
+  var price = parsed_json['query']['results']['quote']['Ask'];
 $("#stock_price").html(price);
   }
   });
-    
 
+/*
 $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22FB%22)%0A%09%09&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=", function(data) {
     var ask = data.query.results.quote.ask;
     console.log(ask);
     });
-    
+
     */
-    
-    
-    
+
+
+
 });
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
