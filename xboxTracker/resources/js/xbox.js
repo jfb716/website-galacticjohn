54036f72e31ea2a96ceb670c63a3750a3689e7ad
John Black <john.f.black@icloud.com>
2017-03-12 14:51:52 -0400

    added current champ to xbox tracker

--- xboxTracker/resources/js/xbox.js
+++ xboxTracker/resources/js/xbox.js
@@ -32,6 +32,8 @@ getTable();
         for(var i = 0; i < response.length; i++)
         html += '<tr><td>' + response[i].dates + '</td><td>' + response[i].game + '</td><td>' + response[i].home + '</td><td>' + response[i].away + '</td><td>' + response[i].winner + '</td><td>' + response[i].wingoals + '</td><td>' + response[i].losegoals + '</td></tr>';
         $('#gameTable tbody').append(html);
+        var curChampObj = {};
+        var winRates = [];
         $('#home').find('option').not(':first').each(function(){
           playerName = $(this).text();
           function counter(playerName){
@@ -83,8 +85,18 @@ getTable();
 
           html = '<tr><td style="color:white" class="bg-inverse">' + playerName + '</td><td>' + homeCount + '</td><td>' + awayCount + '</td><td>' + gamesPlayed + '</td><td class="bg-primary" style="color:white">' + playerWins + '</td><td>' + goalsFor + '</td><td>' + goalsAgainst + '</td><td class="' + pmStyle + '">' + goalsPM + '</td><td class="percent ' + wpStyle + '">' + winPercent.toFixed(0) + '%</td></tr>';
           $('.standingsBody').append(html);
+          var winNum = Number(winPercent);
+          curChampObj[playerName] = winNum;
+          winRates.push(winNum);
         });
-
+        maxWinRate = Math.max.apply(Math, winRates);
+        var champPicker = Object.keys(curChampObj).forEach(function(key){
+          if(maxWinRate === curChampObj[key]){
+            champ = key;
+          }
+        });
+        champHtml = '<div><strong>Current Champ: </strong>' + champ + '</div>';
+        $('.curChamp').append(champHtml);
         }
     });
   }
